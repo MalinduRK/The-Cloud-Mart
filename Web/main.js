24dace9572b24c52c6e1f2b7771c2165417035fe
MalinduRK <malindurk@gmail.com>
2023-03-22 23:54:42 +0530

    Alert and some code cleanup

--- Web/main.js
+++ Web/main.js
@@ -1,10 +1,8 @@
 // Initialize Firebase
 var firebaseConfig = {
-    // your Firebase config here
     apiKey: "AIzaSyBIYvM5ZsdY5C-xH8qIIRDruHs3aPGMW54",
     authDomain: "the-cloud-mart.firebaseapp.com",
     databaseURL: "https://the-cloud-mart-default-rtdb.asia-southeast1.firebasedatabase.app",
-    //databaseURL: "https://the-cloud-mart-default-rtdb.firebaseio.com ",
     projectId: "the-cloud-mart",
     storageBucket: "the-cloud-mart.appspot.com",
     messagingSenderId: "558540507839",
@@ -17,16 +15,12 @@ firebase.initializeApp(firebaseConfig);
 // Get a reference to the Firestore database
 var db = firebase.firestore();
 
-//import { getStorage, ref, uploadBytes } from "firebase/storage";
-
 const storage = firebase.storage().ref();
 
 // Get references to the form input fields
 var itemName = document.getElementById("item-name");
 var itemPrice = document.getElementById("item-price");
 var itemDescription = document.getElementById("item-description");
-//var itemImageUrl = document.getElementById("item-image-url");
-var alertMessage = document.getElementById("alert");
 
 // Handle form submission
 var form = document.getElementById("item-form");
@@ -38,7 +32,6 @@ form.addEventListener("submit", async function(event) {
     var price = parseFloat(itemPrice.value);
     var description = itemDescription.value;
     const file = form.elements.image.files[0];
-    //var imageUrl = itemImageUrl.value;
 
     // Upload the file to Firebase Storage
     const storageRef = firebase.storage().ref().child("item-images/" + file.name);
@@ -56,60 +49,11 @@ form.addEventListener("submit", async function(event) {
     })
     .then(function(docRef) {
         console.log("Item added with ID: ", docRef.id);
-        // optionally redirect to a success page
-        
-        // Change its background color
-        alertMessage.style.display = "block";
+        alert("Successfully added item!");
+        // Reset the form
+        form.reset();
     })
     .catch(function(error) {
         console.error("Error adding item: ", error);
     });
 });
-
-
-
-// firebase storage
-/*
-import { getStorage, ref, uploadBytes } from "firebase/storage";
-
-const storage = getStorage();
-
-<form id="my-form">
-  <input type="text" name="name" placeholder="Item name">
-  <input type="text" name="description" placeholder="Item description">
-  <input type="number" name="price" placeholder="Item price">
-  <input type="file" name="image" accept="image/*">
-  <button type="submit">Add Item</button>
-</form>
-
-const form = document.getElementById("my-form");
-
-form.addEventListener("submit", async (event) => {
-  event.preventDefault();
-
-  const name = form.elements.name.value;
-  const description = form.elements.description.value;
-  const price = form.elements.price.value;
-  const file = form.elements.image.files[0];
-
-  // Upload the file to Firebase Storage
-  const storageRef = ref(storage, "item-images/" + file.name);
-  const snapshot = await uploadBytes(storageRef, file);
-
-  // Get the download URL for the file
-  const imageUrl = await snapshot.ref.getDownloadURL();
-
-  // Save the item data to Firestore
-  const itemRef = await db.collection("items").add({
-    name: name,
-    description: description,
-    price: price,
-    imageUrl: imageUrl
-  });
-
-  console.log("Item added with ID: ", itemRef.id);
-
-  // Reset the form
-  form.reset();
-});
-*/
