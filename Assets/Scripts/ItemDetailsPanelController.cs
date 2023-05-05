297dc86273d060bdcf75f8bf768d5f1c298ac0fb
MalinduRK <malindurk@gmail.com>
2023-05-05 16:30:51 +0530

    Added basic add to cart functionality
    The count goes up each time the user clicks on add to cart in any item detail panel

--- Assets/Scripts/ItemDetailsPanelController.cs
+++ Assets/Scripts/ItemDetailsPanelController.cs
@@ -1,10 +1,15 @@
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ItemDetailsPanelController : MonoBehaviour
 {
+    // Object references
     public GameObject itemDetailsPanel;
     public Text promptText;
+    public TextMeshProUGUI cartText;
+    List<string> cart = new List<string>();
 
     // Start is called before the first frame update
     void Start()
@@ -29,5 +34,11 @@ public class ItemDetailsPanelController : MonoBehaviour
         // Show button prompt when closing the panel
         promptText.text = "View details\n[Left Mouse Button]";
         itemDetailsPanel.SetActive(false);
+    }
+
+    public void AddToCart()
+    {
+        cart.Add("test");
+        cartText.text = $"In Cart: {cart.Count}";
     }
 }
