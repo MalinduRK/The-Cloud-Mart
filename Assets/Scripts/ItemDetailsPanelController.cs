965b447f3926ceec381997a48feb6b74c6dd9e91
MalinduRK <malindurk@gmail.com>
2023-05-05 16:46:07 +0530

    Items are added to the cart with item id now

--- Assets/Scripts/ItemDetailsPanelController.cs
+++ Assets/Scripts/ItemDetailsPanelController.cs
@@ -5,11 +5,16 @@ using UnityEngine.UI;
 
 public class ItemDetailsPanelController : MonoBehaviour
 {
+    public bool customDebug;
+    public bool buttonPressDebug;
     // Object references
     public GameObject itemDetailsPanel;
     public Text promptText;
     public TextMeshProUGUI cartText;
     List<string> cart = new List<string>();
+    // Raycast
+    public Camera mainCamera;
+    public float maxDistance = 2.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -21,11 +26,12 @@ public class ItemDetailsPanelController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
     public void ClosePanel()
     {
+        ButtonPressDebug("Back");
         // Disable mouse
         GameState.HideCursor();
         // Enable camera and player movement
@@ -38,7 +44,30 @@ public class ItemDetailsPanelController : MonoBehaviour
 
     public void AddToCart()
     {
-        cart.Add("test");
+        // Raycast
+        Ray ray = mainCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+        if (Physics.Raycast(ray, out RaycastHit hit, maxDistance))
+        {
+            string objectName = hit.collider.gameObject.name;
+            cart.Add(objectName);
+            CustomDebug($"{objectName} added to cart");
+        }
         cartText.text = $"In Cart: {cart.Count}";
+    }
+
+    private void CustomDebug(string message)
+    {
+        if (customDebug)
+        {
+            Debug.Log(message);
+        }
+    }
+
+    private void ButtonPressDebug(string message)
+    {
+        if (customDebug)
+        {
+            Debug.Log("Button pressed: " + message);
+        }
     }
 }
